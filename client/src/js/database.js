dcc703463823cd44016998b7651f3eeef2d4f612
irivera2116 <irivera2116@GMAIL.COM>
2023-08-20 08:01:13 -0400

    wokred on database.js and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readwrite');
+  const storage = text.objectStore('jate');
+  const request = storage.put({ id: 1, value: content });
+  const response = await request;
+  console.log('Saved to Database!', response);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const text = jateDB.transaction('jate', 'readonly');
+  const storage = text.objectStore('jate');
+  const request = storage.getAll();
+  const response = await request;
+  console.log('Response from Database!', response);
+  return response.value;
+};
 
 initdb();
