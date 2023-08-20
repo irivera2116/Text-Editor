8fe37af062399749f054995bf05d5ad1481c7347
irivera2116 <irivera2116@GMAIL.COM>
2023-08-20 19:56:13 -0400

    added dependancies, fixed config and added to src

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,10 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -18,7 +21,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text-Editor',
+        short_name: 'J.A.T.E',
+        description: 'offline text editor',
+        background_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
